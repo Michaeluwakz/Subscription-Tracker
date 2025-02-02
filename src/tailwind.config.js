c5b3f6a48a21c47ea823b833a9e95ca0d39cdfc0
MichaelUwakz <90646867+Michaeluwakz@users.noreply.github.com>
2025-02-02 18:34:56 +0100

    Add files via upload

---
+++ src/tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./src/components.{js,jsx,ts,tsx}", // Include all JS/JSX/TS/TSX files in the src folder
+    ],
+    theme: {
+      extend: {}, // Extend or customize the default Tailwind theme here
+    },
+    plugins: [], // Add Tailwind plugins here if needed
+  };
